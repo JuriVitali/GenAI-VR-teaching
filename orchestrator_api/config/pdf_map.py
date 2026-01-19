3612ba52a1d0c305f38884e11184b6da4fcfaa2f
leobordoo <bordonileonardo@gmail.com>
2026-01-19 16:10:33 +0000

    Error and bug fix

--- orchestrator_api/config/pdf_map.py
+++ orchestrator_api/config/pdf_map.py
@@ -1,6 +1,6 @@
 import os
 
-PDF_DIR = os.getenv("PDF_DIR", "/home/vrai/Leo/GenAI-VR-teaching/pdfs")
+PDF_DIR = os.getenv("PDF_DIR", "/home/vrai/Leo/GenAI-VR-teaching/shared/knowledge")
 
 PDF_MAP = {
     "pdf1": os.path.join(PDF_DIR, "pdf1.pdf"),
